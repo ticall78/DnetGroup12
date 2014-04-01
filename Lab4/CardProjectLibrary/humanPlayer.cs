5ff0588c6bb4f702f4e4f9f3a6cd04810cce2a5b
stephkaal <xo-steph7@hotmail.com>
2014-04-01 12:11:10 -0400

    updated guii

---
+++ Lab4/CardProjectLibrary/humanPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PeterCardLibrary;
+
+namespace PeterCardLibrary
+{
+    public class humanPlayer
+    {
+        int humanWins = 0;
+
+
+    }
+}
