6ec835f9fe6c801ef74aefbb5bc7ea1b891723ac
stephkaal <xo-steph7@hotmail.com>
2014-04-04 10:29:57 -0400

    updated

---
+++ Lab42/CardProjectLibrary/Deck.cs
@@ -0,0 +1,108 @@
+﻿// Deck Class ** Not used in this particular lab, but for final project- please disregard
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using PeterCardLibrary;
+
+namespace PeterCardLibrary
+{
+    // Make a enum for the different deck sizes(20, 36, 52)
+
+    public class Deck
+    {
+        private PlayingCard[] cards; 
+
+
+        public Deck(int numOfCards = 52)
+        {
+            if (numOfCards == 52)
+            {
+                cards = new PlayingCard[52];
+                for (int suitVal = 0; suitVal < 4; suitVal++)
+                {
+                    for (int rankVal = 1; rankVal < 14; rankVal++)
+                    {
+                        cards[suitVal * 13 + rankVal - 1] = new PlayingCard((Rank)suitVal,
+                                                                    (Suit)rankVal);
+                    }
+                }
+            }
+            
+            else if (numOfCards == 36)
+            {
+                 cards = new PlayingCard[36];
+                for (int suitVal = 0; suitVal < 4; suitVal++)
+                {
+                    for (int rankVal = 6; rankVal < 14; rankVal++)
+                    {
+                        cards[suitVal * 8 + rankVal - 6] = new PlayingCard((Rank)suitVal,
+                                                                    (Suit)rankVal);
+                    }
+                }
+             }
+                
+                else if (numOfCards == 20)
+                {
+                     cards = new PlayingCard[20];
+                    for (int suitVal = 0; suitVal < 4; suitVal++)
+                    {
+                        for (int rankVal = 10; rankVal < 14; rankVal++)
+                        {
+                            cards[suitVal * 5 + rankVal - 10] = new PlayingCard((Rank)suitVal,
+                                                                        (Suit)rankVal);
+                        }
+                     }
+                 }
+    
+                else
+                {
+                    Console.Write("Please enter a : ");
+                }
+                
+          }       
+       
+
+        public PlayingCard GetCard(int cardNum)
+        {
+            if (cardNum >= 0 && cardNum < cards.Length)
+                return cards[cardNum];
+             
+            else
+                throw (new System.ArgumentOutOfRangeException("cardNum", cardNum,
+                          "Value must be between 0 and 51."));
+        }
+
+        public void Shuffle()
+        {
+            PlayingCard[] newDeck = new PlayingCard[cards.Length];
+            bool[] assigned = new bool[cards.Length];
+            Random sourceGen = new Random();
+            for (int i = 0; i < 52; i++)
+            {
+                int destCard = 0;
+                bool foundCard = false;
+                while (foundCard == false)
+                {
+                    destCard = sourceGen.Next(cards.Length);
+                    if (assigned[destCard] == false)
+                        foundCard = true;
+                }
+                assigned[destCard] = true;
+                newDeck[destCard] = cards[i];
+            }
+            newDeck.CopyTo(cards, 0);
+        }
+
+        // Clone Method
+        // Supports the IClonable interface.  Used for deep copying in card collection classes
+        public object Clone()
+        {
+            return this.MemberwiseClone();// Return a memberwise clone
+        } // End of Clone
+
+    }
+
+}
---
+++ Lab42/DemoTester/DemoTester.cs
@@ -0,0 +1,104 @@
+﻿/* Lab Name:       Lab 4- Windows Form Control
+  * File Name:     DemoTester.cs 
+  * Name:          Peter Kalo 
+  * Student ID:    100503976
+  * Date:          March 17th 2014
+  * Description:   Creating a windows form project that goes hand in hand with a card library
+  *                as well as a card box, both of which are referenced so they can be accessible.
+  *                With the aid of event handlers, functionality of the forms Card can be clicked
+  *                so that it has the orientation required as well as ensuring wehter it is faced up
+  *                or down.  There are also two comboboxes that work with the cardbox to determine
+  *                the rank and suit selected by the user. Once the button is clicked after selection
+  *                is made, the appropriate card is dispalyed with the proper orientation when clicked,
+  *                as well as two labels which display the state of the card and also the number of
+  *                times the card is flipped in total increaseing with each selection.  
+  */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PeterCardLibrary; // Referenced card library used
+
+namespace DemoTester // Name of the project
+{
+    public partial class frmDemoTester : Form
+    {
+        // Default constructor
+        public frmDemoTester()
+        {
+            InitializeComponent();
+        }
+
+        // Generates a click event so when program runs the text for the label 
+        // Populates a specific action when clicked upon
+        private void cbxTestCard_Click(object sender, EventArgs e)
+        {
+             lblClicked.Text = cbxTestCard.ToString() + " was clicked last.";  
+           
+            // If else statement to show the orientation of the card when clicked upon
+            // Will show vertically
+            if (cbxTestCard.CardOrientation == Orientation.Horizontal)
+                cbxTestCard.CardOrientation = Orientation.Vertical;
+
+            else
+                // Else will show Horizontally when clicked
+                cbxTestCard.CardOrientation = Orientation.Horizontal;
+
+           
+        }
+        
+
+        // Event handler for the button when clicked to make the card face up or show the rank and suit
+        private void btnFlipCard_Click(object sender, EventArgs e)
+        {
+            cbxTestCard.FaceUp = !cbxTestCard.FaceUp;
+        }
+
+        // Changes the suit of the card control to the selected index for the Enum suit.
+        private void cboSuit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbxTestCard.Suit = (PeterCardLibrary.Suit)cboSuit.SelectedIndex;
+        }
+
+        // Changes the suit of the card control to the selected index for the Enum rank.
+        private void cboRank_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbxTestCard.Rank = (PeterCardLibrary.Rank)cboRank.SelectedIndex + 1;
+        }
+
+        // Counter for how many times the card flips
+        private int flipCount = 0;        
+        // Event handler for the card to flip 
+        private void cbxTestCard_CardFlipped(object sender, EventArgs e)
+        {
+            flipCount++; // increases the counter
+    
+            lblFlipped.Text = "Card flip: " + flipCount.ToString(); //output to the label the amount of times the card flips
+
+        }
+
+        // 
+        private void frmDemoTester_Load(object sender, EventArgs e)
+        {
+            // Will populated the combo box witht he values in the enum for Rank
+            cboRank.DataSource = Enum.GetValues(typeof(Rank));
+            // Will populated the combo box witht he values in the enum for Suit
+            cboSuit.DataSource = Enum.GetValues(typeof(Suit));
+
+            // Will specify the cards rank and suit on form load
+            PlayingCard theCard = new PeterCardLibrary.PlayingCard(Rank.Ace, Suit.Spades);
+            //  Card will be face up on form load
+            theCard.FaceUp = true;
+            // Will set the Test card to the control of the new playing card attributes
+            this.cbxTestCard.Card = theCard;
+
+        }   
+             
+    }
+}
