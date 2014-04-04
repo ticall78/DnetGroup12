16016f12d9747d83c9e5a4c8ea362ac60f56349d
stephkaal <xo-steph7@hotmail.com>
2014-04-04 10:57:00 -0400

    stxndzrhfdsd

---
+++ Lab42 - Copy/CardProjectLibrary/Suit.cs
@@ -0,0 +1,18 @@
+﻿// Card Suit Enumeration used
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeterCardLibrary  // Name of the enum
+{
+    public enum Suit : byte
+    {
+        // Suit types
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+}
