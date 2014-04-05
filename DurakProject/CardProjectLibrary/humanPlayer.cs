c0c310efcfa33ccf49e3d802248b7e5ed4299deb
Peter <ticall78@gmail.com>
2014-04-05 03:24:56 -0400

    Form Is working!
    Spent quite a bit of time getting it to work! Stephanie step your game
    up!

---
+++ DurakProject/CardProjectLibrary/humanPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PeterCardLibrary;
+
+namespace PeterCardLibrary
+{
+    public class humanPlayer
+    {
+        private PlayingCard humanCard;
+        private int humanWins;
+        private Deck deck;
+
+
+   //    public PlayGame()
+   //    {
+   //        InitializeComponent();
+   //        humanWins = 0;
+   //        deck = new Deck(1);
+
+   //    }
+
+
+
+        
+
+
+    }
+}
